716e68ae8057fd02f12bf9e16d0d8404e5e681f2
lopez <lopez@mpib-berlin.mpg.de>
2023-04-04 13:31:05 +0200

    switching from notebook to modules - TASK 04042023KB

---
+++ modules/elastic_search.py
@@ -0,0 +1,22 @@
+
+
+queries = [
+    {
+        "description": "search by id",
+        "query": {
+            "bool": {
+                "must": {
+                    "match_phrase": {
+                        "_id": "4011af8ea429e3c113c7328a721f6a2af2fd188f_L5lt6s5MEeqm_pesYHJVVQ",
+                    }
+                },
+                },
+        }
+    },
+    {
+        "description": "search by term in text",
+        "query": {
+
+        }
+    }
+]
