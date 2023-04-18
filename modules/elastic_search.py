dcc9c802014cc4c4c4f42e339d1939681e9f74d3
Hyosang <kim@mpib-berlin.mpg.de>
2023-04-18 14:12:22 +0200

    third commit for pipeline

--- modules/elastic_search.py
+++ modules/elastic_search.py
@@ -1,107 +1,67 @@
 from typing import List
+import pandas as pd
 
-queries = [
-    {
-        "description": "search by id",
-        "query": {
-            "bool": {
-                "must": {
-                    "match_phrase": {
-                        "_id": "4011af8ea429e3c113c7328a721f6a2af2fd188f_L5lt6s5MEeqm_pesYHJVVQ",
-                    }
-                },
-            },
-        }
-    },
-    {
-        "description": "search documents by list of values in tags",
-        "query": {
-            "bool": {
-                "must": [
-                    {
-                        "terms": {
-                            "tags": "<LIST_OF_VALUES>"
-                        }
-                    }
-                ]
-            }
-        }
-    },
-    {
-        "description": "search document by document type",
-        "query": {
-            "bool": {
-                "must": [
-                    {
-                        "terms": {
-                            "tags": ["is:<TYPE>"]
-                        }
-                    }
-                ]
-            }
-        }
-    },
-    {
-        "description": "search documents in date range (where date is in 'text' field when the tags contain 'has:date' or 'has:date-approx'",
-        "query": {
-            "range": {
-                "text": {
-                    "gte": "<BEGIN_DATE>",
-                    "lte": "<END_DATE>"
-                }
-            }
-        }
-
+# Definining subqueries
+query_text = {
+    "match": {
+        "text": "string"
     }
-]
+}
+# Is it 'match' or 'match_phrase' or something else?
 
-_queries = [
-    {
-        "query": {
-            "bool": {
-                "must": [
-                    {
-                        "range": {
-                            "date": {
-                                "gte": "2022-01-01",
-                                "lte": "2022-12-31"
-                            }
-                        }
-                    },
-                    {
-                        "terms": {
-                            "tags": ["tag1", "tag2", "tag3"]
-                        }
-                    }
-                ]
-            }
+query_date_range = {
+    
+    "range": {
+        
+        "text": {    
+            "gte": "begin_date", 
+            "lte": "end_date"
         }
     }
+    
+}
+# Is it 'text' or something else? 
 
-]
-
-query_text = {}
-query_date_range = {}
 query_type = {
     "terms": {
-        "tags": "<LIST_TYPES>"
+        "tags": "<LIST_TYPE>"
+    }
+}
+
+query_ann_type = {   
+    "terms" : {
+        "tags": "<LIST_ANN_TYPES>"
+    }
+}
+
+query_has_property = {
+    "terms": {
+        "tags": "<LIST_PROPERTY>"
     }
 }
 
 
-def search_documents(text: str, date_range: List[str, str], type: str, ann_type, has_property: str,
-                     keywords: List[str]) -> list:
+query_keywords = {
+    "terms": {
+        "tags": "TERM_TAGS"
+    }
+}
+
+
+def search_documents(text: str = None , date_range: List[str] = None , type_: str = None, ann_type = None, has_property: str = None,
+                     keywords: List[str] = None) -> list:
     """
     Building a query for elastic search consisting of six subqueries, depending on what's provided
 
     :param text: free text to be search in the fields 'text' or 'document'
     :param date_range: a list with two values [begin_date, end_date]. When provided, we need to filter documents that have date and whose date is in the range
-    :param type: relates to documents which have a tag "is:<TYPE>", where "<TYPE>" is the type provided
+    :param type_: relates to documents which have a tag "is:<TYPE>", where "<TYPE>" is the type provided
     :param ann_type: relates to documents which have a tag "ann:<ANN_TYPE>", where "<ANN_TYPE>" is the ann_type provided
     :param has_property: relates to documents which have a tag "has:<HAS_PROPERTY>", where "<HAS_PROPERTY>" is the has_property provided
     :param keywords: list of terms expected to be found in the "tags" field ONLY and they appear without any specification (not "is:", "has:, "ann:")
     :return: a list of results with all relevant documents for that query
     """
+    
     query = {
         "query": {
             "bool": {
@@ -111,21 +71,34 @@ def search_documents(text: str, date_range: List[str, str], type: str, ann_type,
     }
 
     if text:
-        pass
+        query_text['match']['text'] = text
+        query['query']['bool']['must'].append(query_text)
 
     if date_range:
-        pass
+        query_date_range['range']['text']['gte'] = date_range[0]
+        query_date_range['range']['text']['lte'] = date_range[1]
+        query['query']['bool']['must'].append(query_date_range) #'query' - 'range' should be? or is it okay?    
 
-    if type:
-        pass
-        query_type['terms']['tags'] = [type]
+        
+    if type_:
+        query_type['terms']['tags'] = ["is:"+ type_]
         query['query']['bool']['must'].append(query_type)
+        
+    if ann_type: 
+        query_ann_type['terms']['tags'] = ["ann:"+ann_type]
+        query['query']['bool']['must'].append(query_ann_type)
+    
+    if has_property: 
+        query_has_property['terms']['tags'] = ["has:"+has_property]
+        query['query']['bool']['must'].append(query_has_property)
+    
+    if keywords: 
+        query_keywords['terms']['tags'] = keywords
+        query['query']['bool']['must'].append(query_keywords)
+    
 
-
-    res = es.search(index="hypothesis_v1", body={"query": query})
+    res = es.search(index="hypothesis_v1", body={"query": query['query']})
     for doc in res['hits']['hits']:
         print("%s) %s" % (doc['_source']['user'], doc['_source']['document']))
 
     return res['hits']['hits']
-
-# search_documents(text="vaccination measures", date_range=["2020-05-01", "2020-06-01"], type="tweet", ann_type="doi", has_property="context", keywords=["COVID"])
